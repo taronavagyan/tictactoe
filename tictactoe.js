8a7e2f0d4db2548e0895d984c56ee17b9f99e2dd
Taron <88250538+taronavagyan@users.noreply.github.com>
2022-05-21 21:26:55 -0700

    tictactoe

---
+++ tictactoe.js
@@ -0,0 +1,30 @@
+function displayBoard(board) {
+  if (typeof board !== "object") board = initalizeBoard();
+  console.log("");
+  console.log("     |     |");
+  console.log(`  ${board["1"]}  |  ${board["2"]}  |  ${board["3"]}`);
+  console.log("     |     |");
+  console.log("-----+-----+-----");
+  console.log("     |     |");
+  console.log(`  ${board["4"]}  |  ${board["5"]}  |  ${board["6"]}`);
+  console.log("     |     |");
+  console.log("-----+-----+-----");
+  console.log("     |     |");
+  console.log(`  ${board["7"]}  |  ${board["8"]}  |  ${board["9"]}`);
+  console.log("     |     |");
+  console.log("");
+}
+
+function initalizeBoard() {
+  let board = {};
+
+  for (let count = 1; count <= 9; count += 1) {
+    board[count] = " ";
+  }
+  return board;
+}
+
+let board = initalizeBoard();
+displayBoard(board);
+
+displayBoard({ b: "b" });
