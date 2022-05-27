fb964c423468f7c6065bbc6548bbaa03b15dee09
Taron Avagyan <contact@taronavagyan.com>
2022-05-26 23:24:18 -0700

    added constnats

--- tictactoe.js
+++ tictactoe.js
@@ -1,3 +1,15 @@
+const readline = require("readline-sync");
+
+const INITIAL_MARKER = " ";
+const HUMAN_MARKER = "X";
+const COMPUTER_MARKER = "O";
+//prettier-ignore
+const WINNING_LINES = [
+  [1, 2, 3], [4, 5, 6], [7, 8, 9], // rows
+  [1, 4, 7], [2, 5, 8], [3, 6, 9], // columns
+  [1, 5, 9], [3, 5, 7], // diagonals
+];
+
 function displayBoard(board) {
   if (typeof board !== "object") board = initalizeBoard();
   console.log("");
